dd10dfb49667c85e48544487d22acc866f4650bf
mikespriggs26 <mikespriggs@msn.com>
2018-03-25 15:18:52 -0400

    Final Updates

--- BerwynCodingEval/Program.cs
+++ BerwynCodingEval/Program.cs
@@ -11,6 +11,23 @@ namespace BerwynCodingEval
     {
         static void Main(string[] args)
         {
+            //            Build a C# console application to parse this file and output a result .csv file. 
+
+            //C# Console Output: 
+            //•             Output the total number of records in the file. 
+            //•             Show the largest sum of Val1 and Val2 for any single row in the CSV, as well as the GUID for that row. 
+            //•             Show any Duplicate GUID values.
+            //•             Show the average length of Val3 across all input rows.
+            //Output CSV file: 
+            //•             For each input row: 
+            //•             Output columns: 
+            //•             GUID 
+            //•             Val1 + Val2 
+            //•             IsDuplicateGuid(Y or N) 
+            //•             Is Val3 length greater than the average length of Val3(Y or N)
+
+
+
             int ValMax = 0;
             string greatestGUID = "";
             int ValSum = 0;
@@ -20,10 +37,12 @@ namespace BerwynCodingEval
             int Val2Int = 0;
             var lines = "";
 
+
             List<string> listGUID = new List<string>();
             List<string> listVal1 = new List<string>();
             List<string> listVal2 = new List<string>();
             List<string> listVal3 = new List<string>();
+           
             
             using (var reader = new StreamReader("test.csv"))
             {
@@ -66,7 +85,6 @@ namespace BerwynCodingEval
                 }
                 
             }
-
             Console.WriteLine("The greatest sum of Val1 and Val2 is: "+ ValMax);
             Console.WriteLine("The corresponding GUID is: " + greatestGUID);
             
@@ -86,9 +104,14 @@ namespace BerwynCodingEval
                 if (!hashset.Add(item))
                 {
                     duplicates.Add(item);
-                    Console.WriteLine(item.Replace('"', ' ').Trim() + " is a duplicate GUID.");
+                    //Console.WriteLine(item.Replace('"', ' ').Trim() + " is a duplicate GUID.");
 
                 }
+            }
+            Console.WriteLine("The duplicate GUIDs are: ");
+            foreach (var dup in duplicates)
+            {
+                Console.WriteLine(dup.Replace('"', ' ').Trim());
             }
             //calculates the average of all Val3 entries
             int listVal3Length = 0;
@@ -99,70 +122,62 @@ namespace BerwynCodingEval
                 count++;
                 
             }
-            Console.WriteLine("The average is: " + listVal3Length/count);
+            Console.WriteLine("The average is length of Val3 is: " + listVal3Length/count);
 
 
+            //Outputting the new file
             StreamWriter writer = new StreamWriter("outputFile.csv");
-            //using (writer)
-            //{
-            //for (int i = 0; i < 4; i++)
-            int counter = 0;
-            foreach(var line in lines)
+            using (writer)
+
+            for (int i = 1; i < listGUID.Count; i++)
+            
             {
-                Val1Value = listVal1[line].Replace('"', ' ').Trim();
-                Val2Value = listVal2[line].Replace('"', ' ').Trim();
+                Val1Value = listVal1[i].Replace('"', ' ').Trim();
+                Val2Value = listVal2[i].Replace('"', ' ').Trim();
                 Val1Int = Int32.Parse(Val1Value);
                 Val2Int = Int32.Parse(Val2Value);
                 ValSum = Val1Int + Val2Int;
-                //int sum = Int32.Parse(listVal1[line]) + Int32.Parse(listVal2[line]);
-                Console.Write(listGUID[line]);
+                
+                Console.Write(listGUID[i]);
                 Console.Write(",");
+
                 Console.Write(ValSum);
+                Console.Write(",");
+
+                if (duplicates.Contains(listGUID[i]))
+                {
+                    Console.Write("Yes");
+                }
+                else
+                {
+                    Console.Write("No");
+                }
+                Console.Write(",");
+
+                
+                int Val3Length = listVal3[i].Count();
+
+                if (Val3Length > listVal3Length / count)
+                {
+                    Console.Write("Yes");
+                }
+                else
+                {
+                    Console.Write("No");
+                }
+
                 Console.WriteLine();
-                Console.WriteLine(counter);
-                counter++;
+                
+               
                    
             }
 
-
-            //}
-
+            
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-            //checking for largest Val1
-            //int highVal1 = 0;
-            //foreach (var item in listA)
-            //{
-            //    //item.Trim('"');
-            //    for (int i = 2; i < listB.Count; i++)
-            //    {
-
-            //        for (int j = 2; j < listB.Count; j++)
-            //        {
-            //            if (Int32.Parse(item) > highVal1)
-            //            {
-            //                highVal1 = Int32.Parse(item);
-            //            }
-
-
-            //        }
-            //    }
-            //}
-            //Console.WriteLine(highVal1);
+            
 
 
 
